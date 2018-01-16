50e1afe6b60e8692a85b7ab23cdbb78cf511faa3
Michelle Ranly <meranly8@gmail.com>
2018-01-16 15:48:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function theBeatlesPlay(musicians,instruments) {
   const array = []
   
   for (let i = 0; i < musicians.length; i++ ){
+    musician = musicians[i]
+    instrument = instruments[i[]]
     array.push(`${musicians} plays ${instruments}`)
   }
   return array
