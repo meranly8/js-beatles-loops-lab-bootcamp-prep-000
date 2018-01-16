42a14c490d184894fdb49a558bda7433d672c431
Michelle Ranly <meranly8@gmail.com>
2018-01-16 15:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function johnLennonFacts(array){
 
 function iLoveTheBealtes(number) {
   var array = []
-  do {array.push("I love the Beatles!")
+  do {
+    array.push("I love the Beatles!")
+    number++
   } while {
     
   } 
